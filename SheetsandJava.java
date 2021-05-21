752b7ba425c6bc96b4949a05d2a9fe5505677200
jhurta13 <78060658+jhurta13@users.noreply.github.com>
2021-05-20 19:30:44 -0700

    Add files via upload
    SheetsandJava class is used to communicate with Google Sheets via Java. Google sheets will house the master rules doc

---
+++ SheetsandJava.java
@@ -0,0 +1,57 @@
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
+
+public class SheetsandJava {
+    private static Sheets sheetsService;
+    private static String APPLICATION NAME = "Google Sheets Example";
+    private static String SPREADSHEET ID = "1RIWYj7gNcOrhmtGT10EcM5BydrmHCyKIQwLXvEazKfI";
+
+    private static Credential authorize() throws IOException, GeneralSecurityException {
+        InputStream in = SheetsandJava.class.getResourceStream("/credentials.json");
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(
+                JacksonFactory.getDefaultInstance(), new InputStreamReader(in)
+        );
+
+        List<String> scopes = Arrays.asList(SheetsScopes.SPREADSHEETS);
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(),
+                clientSecrets, scopes)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File("tokens")))
+                .build();
+        Credential credential = new AuthorizationCodeInstalledApp(
+                flow, new LocalServerReceiver())
+                .authorize("user");
+        return credential;
+    }
+    public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
+        Credential credential = authorize();
+        return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(),
+                JackonFactory.getDefaultInstance(),credential)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        }
+        public static void main (String[] args) throws IOException, GeneralSecurityException {
+
+        }
+
+}
