30d52d10ddf47591bd094f4206bad6905dc01cee
jhurta13 <78060658+jhurta13@users.noreply.github.com>
2021-06-03 21:27:47 -0700

    Add files via upload
    Working on implementing rules modifications by user and client agent communications

--- AgentServer.java
+++ AgentServer.java
@@ -1,71 +1,71 @@
 /** File is: InetServer.java, Version 1.9
 
- A multithreaded server for AgentClient. Elliott, after Hughes, Shoffner, Winslow
+A multithreaded server for AgentClient. Elliott, after Hughes, Shoffner, Winslow
 
- You will need at least TWO separate command windows to run this N > 1 process program.
+You will need at least TWO separate command windows to run this N > 1 process program.
 
- ShellA> java AgentServer
- ShellB> java AgentClient
+ShellA> java AgentServer
+ShellB> java AgentClient
 
- Optionally, to run across the Internet:
+Optionally, to run across the Internet:
 
- ShellB> java AgentClient YourDomainNameOfOtherMachine
+ShellB> java AgentClient YourDomainNameOfOtherMachine
 
- You can run 5,000 AgentClients if you wish. Each will have a separate conversation with its Worker.
+You can run 5,000 AgentClients if you wish. Each will have a separate conversation with its Worker.
 
- Windows Bat File:
+Windows Bat File:
 
- > start java AgentServer
- > start java AgentClient
- ----------------------------------------------------------------------*/
+> start java AgentServer
+> start java AgentClient
+----------------------------------------------------------------------*/
 
 import java.io.*;  // Get the Input Output libraries
 import java.net.*; // Get the Java networking libraries
 
 class Worker extends Thread {    // Class definition. Each connection gets its own Worker
-    Socket sock;                   // Class member, socket, local to Worker.
-    Worker (Socket s) {sock = s;}  // Constructor, assign arg s to local sock
+  Socket sock;                   // Class member, socket, local to Worker.
+  Worker (Socket s) {sock = s;}  // Constructor, assign arg s to local sock
 
-    public void run(){
-        // Get I/O streams in/out from the socket:
-        PrintStream out = null;
-        BufferedReader in = null;
-        try {
-            in = new BufferedReader
-                    (new InputStreamReader(sock.getInputStream()));
-            out = new PrintStream(sock.getOutputStream());
-            // Note that this branch might not execute when expected:
-            try {
-                String ClientMsg;
-                ClientMsg = in.readLine ();
-                System.out.println("Got: " + ClientMsg);
+  public void run(){
+    // Get I/O streams in/out from the socket:
+    PrintStream out = null;
+    BufferedReader in = null;
+    try {
+      in = new BufferedReader
+        (new InputStreamReader(sock.getInputStream()));
+      out = new PrintStream(sock.getOutputStream());
+      // Note that this branch might not execute when expected:
+      try {
+        String ClientMsg;
+        ClientMsg = in.readLine ();
+        System.out.println("Got: " + ClientMsg);
 
-                out.println("Hi. I am your Agent."); // To client...
-                out.println("You said: " + ClientMsg + "\n\n");
-            } catch (IOException x) {
-                System.out.println("Server read error");
-                x.printStackTrace ();
-            }
-            sock.close(); // close this connection, but not the server;
-        } catch (IOException ioe) {System.out.println(ioe);}
-    }
+	out.println("Hi. I am your Agent."); // To client...
+	out.println("You said: " + ClientMsg + "\n\n");
+      } catch (IOException x) {
+        System.out.println("Server read error");
+        x.printStackTrace ();
+      }
+      sock.close(); // close this connection, but not the server;
+    } catch (IOException ioe) {System.out.println(ioe);}
+  }
 }
 
 public class AgentServer {
-
-    public static void main(String a[]) throws IOException {
-        int q_len = 6; /* Not interesting. # of simultaneous requests for OpSys to queue */
-        int port = 45565;
-        Socket sock; // This will hold the new connection.
-
-        ServerSocket servsock = new ServerSocket(port, q_len); // Create a doorbell socket
-
-        System.out.println
-                ("Film Coverage Agent server 1.9 starting up, listening at port 45565.\n");
-        while (true) {
-            sock = servsock.accept(); // Wait for the next client connection. Ding-Dong! Hello?
-            new Worker(sock).start(); // Spawn worker to handle it, and start the thread.
-        }
+  
+  public static void main(String a[]) throws IOException {
+    int q_len = 6; /* Not interesting. # of simultaneous requests for OpSys to queue */
+    int port = 45565;
+    Socket sock; // This will hold the new connection.
+    
+    ServerSocket servsock = new ServerSocket(port, q_len); // Create a doorbell socket
+    
+    System.out.println
+      ("Clark Elliott's Agent server 1.9 starting up, listening at port 45565.\n");
+    while (true) {
+      sock = servsock.accept(); // Wait for the next client connection. Ding-Dong! Hello?
+      new Worker(sock).start(); // Spawn worker to handle it, and start the thread.
     }
+  }
 }
 
