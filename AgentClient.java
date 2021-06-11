5b078cbe88663d3b3ca30c23911d7797248eddda
jhurta13 <78060658+jhurta13@users.noreply.github.com>
2021-06-10 21:03:11 -0700

    Final Submission
    *To Do:
    1. If statements not working correctly

--- AgentClient.java
+++ AgentClient.java
@@ -1,80 +1,123 @@
 /** File is: AgentClient.java, Version 1.9
 
-    A client for AgentServer. Elliott, after Hughes, Shoffner, Winslow
+ A client for AgentServer. Elliott, after Hughes, Shoffner, Winslow
 
-You will need at least TWO separate command windows to run this N > 1 process program.
+ You will need at least TWO separate command windows to run this N > 1 process program.
 
-ShellA> java AgentServer
-ShellB> java AgentClient
+ ShellA> java AgentServer
+ ShellB> java AgentClient
 
-Optionally, to run across the Internet:
+ Optionally, to run across the Internet:
 
-ShellB> java AgentClient YourDomainNameOfOtherMachine
+ ShellB> java AgentClient YourDomainNameOfOtherMachine
 
-You can run 5,000 AgentClients if you wish. Each will have a separate conversation with its Worker.
+ You can run 5,000 AgentClients if you wish. Each will have a separate conversation with its Worker.
 
-Windows Bat File:
+ Windows Bat File:
 
-> start java AgentServer
-> start java AgentClient
-----------------------------------------------------------------------*/
+ > start java AgentServer
+ > start java AgentClient
+ ----------------------------------------------------------------------*/
 
-import java.io.*; // Get the Input Output libraries
-import java.net.*; // Get the Java networking libraries
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class AgentClient{
-  public static void main (String args[]) {
-    String serverName;
-    if (args.length < 1) serverName = "localhost";
-    else serverName = args[0];
-    
-    System.out.println("Clark Elliott's Agent Client, 1.9.\n");
-    System.out.println("Using server: " + serverName + ", Port: 45565");
-    BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    try {
-      String name;
-      do {
-        System.out.print
-          ("Enter a string to send to the Agent, (quit) to end: ");
-        System.out.flush ();
-        name = in.readLine ();
-        if (name.indexOf("quit") < 0)
-          getAgentResponse(name, serverName);
-      } while (name.indexOf("quit") < 0); // quitSmoking.com?
-      System.out.println ("Cancelled by user request.");
-    } catch (IOException x) {x.printStackTrace ();}
-  }
-  
-  
-  static void getAgentResponse (String name, String serverName){
-    Socket sock;
-    BufferedReader fromServer;
-    PrintStream toServer;
-    String textFromServer;
-    
-    try{
-      /* Open our connection to server port, choose your own port number.. */
-      sock = new Socket(serverName, 45565);
-      
-      // Create filter I/O streams for the socket:
-      fromServer =
-        new BufferedReader(new InputStreamReader(sock.getInputStream()));
-      toServer = new PrintStream(sock.getOutputStream());
+    public static void main (String args[]) throws IOException {
+        String serverName;
+        if (args.length < 1) serverName = "localhost";
+        else serverName = args[0];
 
-      // Send the Agent Server your string:
-      toServer.println(name);
-      toServer.flush();
-      
-      // Read two or three lines of response from the server,
-      // and block while synchronously waiting:
-      for (int i = 1; i <=3; i++){
-        textFromServer = fromServer.readLine();
-        if (textFromServer != null) System.out.println(textFromServer);
-      }
-      sock.close();
-    } catch (IOException x) {
-      System.out.println ("Socket error.");
-      x.printStackTrace ();
+        System.out.println("Screenplay Coverage Agent Client, 1.9.\n");
+        System.out.println("Using server: " + serverName + ", Port: 45565");
+        String name;
+        do {
+
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Is there a CATALIST? (Y/N) \n");
+
+            String answer1 = myObj.nextLine();  // Read user input
+            System.out.println("CATALIST: " + answer1);  // Output user input
+
+            System.out.println("Is the BALANCE upset? (Y/N) \n");
+
+            String answer2 = myObj.nextLine();  // Read user input
+            System.out.println("BALANCE upset: " + answer2);  // Output user input
+
+            System.out.println("Is the Protagonist's DESIRE established? (Y/N) \n");
+
+            String answer3 = myObj.nextLine();  // Read user input
+            System.out.println("DESIRE established: " + answer3);  // Output user input
+
+            System.out.println("Is the Protagonist's PROBLEM established? (Y/N) \n");
+
+            String answer4 = myObj.nextLine();  // Read user input
+            System.out.println("Protagonist's PROBLEM established: " + answer4);  // Output user input
+
+            System.out.println("Is the Protagonist's NEED established? (Y/N)\n");
+
+            String answer5 = myObj.nextLine();  // Read user input
+            System.out.println("Protagonist's NEED established: " + answer5);
+
+            System.out.println("Is the Protagonist's GOAL established? (Y/N)\n");
+
+            String answer6 = myObj.nextLine();  // Read user input
+            System.out.println("GOAL established: " + answer6);
+
+            System.out.println("Is the Protagonist's MISSION established? (Y/N)\n");
+
+            String answer7 = myObj.nextLine();  // Read user input
+            System.out.println("Protagonist's MISSION established: " + answer7);
+
+            String answercompiled = answer1+answer2+answer3+answer4+answer5+answer6+answer7;
+
+
+            //send to server for evaluation
+            Reader inputString = new StringReader(answercompiled);
+            BufferedReader in = new BufferedReader(inputString);
+            name = in.readLine();
+
+
+
+            if (name.indexOf("quit") < 0)
+                getAgentResponse(name, serverName);
+        } while (name.indexOf("quit") < 0); // quitSmoking.com?
+        System.out.println ("Cancelled by user request.");
+
+
     }
-  }
+
+
+    static void getAgentResponse (String name, String serverName){
+        Socket sock;
+        BufferedReader fromServer;
+        PrintStream toServer;
+        String textFromServer;
+
+        try{
+            /* Open our connection to server port, choose your own port number.. */
+            sock = new Socket(serverName, 45565);
+
+            // Create filter I/O streams for the socket:
+            fromServer =
+                    new BufferedReader(new InputStreamReader(sock.getInputStream()));
+            toServer = new PrintStream(sock.getOutputStream());
+
+            // Send the Agent Server your string:
+            toServer.println(name);
+            toServer.flush();
+
+            // Read two or three lines of response from the server,
+            // and block while synchronously waiting:
+            for (int i = 1; i <=3; i++){
+                textFromServer = fromServer.readLine();
+                if (textFromServer != null) System.out.println(textFromServer);
+            }
+            sock.close();
+        } catch (IOException x) {
+            System.out.println ("Socket error.");
+            x.printStackTrace ();
+        }
+    }
 }
--- AgentServer.java
+++ AgentServer.java
@@ -1,71 +1,152 @@
 /** File is: InetServer.java, Version 1.9
 
-A multithreaded server for AgentClient. Elliott, after Hughes, Shoffner, Winslow
+ A multithreaded server for AgentClient. Elliott, after Hughes, Shoffner, Winslow
 
-You will need at least TWO separate command windows to run this N > 1 process program.
+ You will need at least TWO separate command windows to run this N > 1 process program.
 
-ShellA> java AgentServer
-ShellB> java AgentClient
+ ShellA> java AgentServer
+ ShellB> java AgentClient
 
-Optionally, to run across the Internet:
+ Optionally, to run across the Internet:
 
-ShellB> java AgentClient YourDomainNameOfOtherMachine
+ ShellB> java AgentClient YourDomainNameOfOtherMachine
 
-You can run 5,000 AgentClients if you wish. Each will have a separate conversation with its Worker.
+ You can run 5,000 AgentClients if you wish. Each will have a separate conversation with its Worker.
 
-Windows Bat File:
+ Windows Bat File:
 
-> start java AgentServer
-> start java AgentClient
-----------------------------------------------------------------------*/
+ > start java AgentServer
+ > start java AgentClient
+ ----------------------------------------------------------------------*/
 
-import java.io.*;  // Get the Input Output libraries
-import java.net.*; // Get the Java networking libraries
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 class Worker extends Thread {    // Class definition. Each connection gets its own Worker
-  Socket sock;                   // Class member, socket, local to Worker.
-  Worker (Socket s) {sock = s;}  // Constructor, assign arg s to local sock
+    Socket sock;                   // Class member, socket, local to Worker.
+    private static final String ENCODING = "ISO-8859-1";
 
-  public void run(){
-    // Get I/O streams in/out from the socket:
-    PrintStream out = null;
-    BufferedReader in = null;
-    try {
-      in = new BufferedReader
-        (new InputStreamReader(sock.getInputStream()));
-      out = new PrintStream(sock.getOutputStream());
-      // Note that this branch might not execute when expected:
-      try {
-        String ClientMsg;
-        ClientMsg = in.readLine ();
-        System.out.println("Got: " + ClientMsg);
 
-	out.println("Hi. I am your Agent."); // To client...
-	out.println("You said: " + ClientMsg + "\n\n");
-      } catch (IOException x) {
-        System.out.println("Server read error");
-        x.printStackTrace ();
-      }
-      sock.close(); // close this connection, but not the server;
-    } catch (IOException ioe) {System.out.println(ioe);}
-  }
-}
+    Worker(Socket s) {
+        sock = s;
+    }  // Constructor, assign arg s to local sock
 
-public class AgentServer {
-  
-  public static void main(String a[]) throws IOException {
-    int q_len = 6; /* Not interesting. # of simultaneous requests for OpSys to queue */
-    int port = 45565;
-    Socket sock; // This will hold the new connection.
-    
-    ServerSocket servsock = new ServerSocket(port, q_len); // Create a doorbell socket
-    
-    System.out.println
-      ("Clark Elliott's Agent server 1.9 starting up, listening at port 45565.\n");
-    while (true) {
-      sock = servsock.accept(); // Wait for the next client connection. Ding-Dong! Hello?
-      new Worker(sock).start(); // Spawn worker to handle it, and start the thread.
+    public void run() {
+        // Get I/O streams in/out from the socket:
+        PrintStream out = null;
+        BufferedReader in = null;
+
+        Scanner s = null;
+        try {
+            s = new Scanner(new File("C:/users/jhsandoval/Desktop/rules.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        ArrayList<String> list = new ArrayList<String>();
+        while (s.hasNext()){
+            list.add(s.nextLine());
+        }
+        s.close();
+
+        try {
+            in = new BufferedReader
+                    (new InputStreamReader(sock.getInputStream()));
+            out = new PrintStream(sock.getOutputStream());
+            // Note that this branch might not execute when expected:
+            try {
+                String ClientMsg;
+                ClientMsg = in.readLine();
+                out.println("Server Received Responses... Evaluating Screenplay...");
+
+                if(ClientMsg.charAt(0)=='Y'){
+                    ClientMsg = "Great, your screenplay has a Catalyst \n and test";
+
+                    if(ClientMsg.charAt(1)=='Y'){
+                        ClientMsg.concat("GDGDSGDSGD");
+                    }
+                }
+                else{
+                    ClientMsg = "Consider adding a CATALYST to you Screenplay \n";
+                }
+
+                if (ClientMsg.charAt(1)=='Y'){
+                    ClientMsg += "Great, your screenplay has an UPSET BALANCE \n";
+                }
+                else if(ClientMsg.charAt(1)=='N')
+                {
+                    ClientMsg += "Consider UPSETTING THE BALANCE in your Screenplay \n";
+                }
+
+                else if(ClientMsg.charAt(2)=='Y'){
+                    ClientMsg += "Great, your Protagonist has a DESIRE  \n";
+                }
+                else if(ClientMsg.charAt(2) =='N')
+                    {
+                    ClientMsg += "Consider giving your PROTAGONIST a DESIRE \n";
+                }
+
+                else if(ClientMsg.charAt(3)=='Y'){
+                    ClientMsg += "Great, your Protagonist has a PROBLEM  \n";
+                }
+                else if(ClientMsg.charAt(3)=='N'){
+                    ClientMsg += "Consider giving your PROTAGONIST a PROBLEM \n";
+                }
+
+                else if(ClientMsg.charAt(4)=='Y'){
+                    ClientMsg += "Great, your Protagonist has a NEED  \n";
+                }
+                else if(ClientMsg.charAt(4)=='N') {
+                    ClientMsg += "Consider giving your PROTAGONIST a NEED \n";
+                }
+
+                else if(ClientMsg.charAt(5)=='Y'){
+                    ClientMsg += "Great, your Protagonist has a GOAL  \n";
+                }
+                else if (ClientMsg.charAt(5)=='N'){
+                    ClientMsg += "Consider giving your PROTAGONIST a GOAL \n";
+                }
+
+                else if(ClientMsg.charAt(6)=='Y'){
+                    ClientMsg += "Great, your Protagonist has a MISSION  \n";
+                }
+                else if(ClientMsg.charAt(6)=='N'){
+                    ClientMsg += "Consider giving your PROTAGONIST a MISSION \n";
+                }
+
+
+                out.print("Your Screenplay Coverage: \n" + ClientMsg);  // Output user input
+                String message = in.readLine();
+
+            } catch (IOException x) {
+                System.out.println("Server read error");
+                x.printStackTrace();
+            }
+            sock.close(); // close this connection, but not the server;
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
     }
-  }
+
+}
+public class AgentServer {
+
+    public static void main(String a[]) throws IOException {
+
+        int q_len = 6; /* Not interesting. # of simultaneous requests for OpSys to queue */
+        int port = 45565;
+        Socket sock; // This will hold the new connection.
+
+        ServerSocket servsock = new ServerSocket(port, q_len); // Create a doorbell socket
+
+        System.out.println
+                ("Film Coverage Agent server 1.9 starting up, listening at port 45565.\n");
+        while (true) {
+            sock = servsock.accept(); // Wait for the next client connection. Ding-Dong! Hello?
+            new Worker(sock).start(); // Spawn worker to handle it, and start the thread.
+        }
+    }
 }
 
